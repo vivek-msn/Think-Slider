861bf92ed1cb128412f8c53eacc5c3222129fbc4
Vivek <vivek_msn@hotmail.com>
2023-09-26 23:57:29 +0530

    add activate,deactivate,uninstall functions with hooks

--- wp-content/plugins/mv-slider/mv-slider.php
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -47,9 +47,24 @@ if( ! class_exists('MV_Slider' ) ){
             define( 'MV_SLIDER_URL', plugin_dir_url( __FILE__) );
             define( 'MV_SLIDER_VERSION', '1.0.0');
         }
+        
+        public static function activate(){
+            update_option( 'rewrite_rules', '' );
+        }
+
+        public static function deactivate(){
+            flush_rewrite_rules();
+        }
+
+        public static function uninstall(){
+            
+        }
     }
 }
 
 if( class_exists( 'MV_Slider' ) ){
+    register_activation_hook( __FILE__,array( 'MV_SLIDER', 'activate' ) );
+    register_deactivation_hook( __FILE__,array( 'MV_SLIDER', 'deactivate' ) );
+    register_uninstall_hook( __FILE__,array( 'MV_SLIDER', 'uninstall' ) );
     $mv_slider = new MV_Slider();
 }
