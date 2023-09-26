0c8077ee0a81fd97b71521239e751dd6a200553f
Vivek <vivek_msn@hotmail.com>
2023-09-26 17:42:32 +0530

    Make a Plugin Folder Structure and add Plugin Description

---
+++ wp-content/plugins/mv-slider/mv-slider.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: MV Slider
+ * Plugin URI: https://www.wordpress.org/mv-slider
+ * Description: My plugin's description
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: Vivek Saini
+ * Author URI: https://www.ithinkservices.com
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: mv-slider
+ * Domain Path: /languages
+ */
