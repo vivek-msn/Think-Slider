e3bbecb2968215c26e7ffcc9b1935eeb2fdc30d6
Vivek <vivek_msn@hotmail.com>
2023-09-29 18:08:29 +0530

    Add bullet field and style field

--- wp-content/plugins/mv-slider/class.mv-slider-settings.php
+++ wp-content/plugins/mv-slider/class.mv-slider-settings.php
@@ -13,12 +13,19 @@ if( ! class_exists( 'MV_Slide_Settings' )){
         public function admin_init(){
 
             register_setting( 'mv_slider_group', 'mv_slider_options' );
-            
+
             add_settings_section(
                 'mv_slider_main_section',
                 'How does it work?',
                 null,
                 'mv_slider_page1'
+            );
+
+            add_settings_section(
+                'mv_slider_second_section',
+                'Other Plugin Options',
+                null,
+                'mv_slider_page2'
             );
 
             add_settings_field(
@@ -28,6 +35,30 @@ if( ! class_exists( 'MV_Slide_Settings' )){
                 'mv_slider_page1',
                 'mv_slider_main_section'
             );
+
+            add_settings_field(
+                'mv_slider_title',
+                'Slider Title',
+                array( $this, 'mv_slider_title_callback'),
+                'mv_slider_page2',
+                'mv_slider_second_section'
+            );
+
+            add_settings_field(
+                'mv_slider_bullets',
+                'Display Bullets',
+                array( $this, 'mv_slider_bullets_callback'),
+                'mv_slider_page2',
+                'mv_slider_second_section'
+            );
+
+            add_settings_field(
+                'mv_slider_style',
+                'Slider Style',
+                array( $this, 'mv_slider_style_callback'),
+                'mv_slider_page2',
+                'mv_slider_second_section'
+            );
         }
 
         public function mv_slider_shortcode_callback(){
@@ -35,5 +66,50 @@ if( ! class_exists( 'MV_Slide_Settings' )){
             <span>Use the Shortcode [mv_slider] to display the slider in any page/post/widget</span>
             <?php
         }
+
+        public function mv_slider_title_callback(){
+            ?>
+            
+                <input 
+                type="text" 
+                name="mv_slider_options[mv_slider_title]" 
+                id="mv_slider_title" 
+                value="<?php echo isset( self::$options['mv_slider_title']) ? esc_attr( self::$options['mv_slider_title'] ) : '';?>"
+                >
+            <?php
+        }
+        
+        public function mv_slider_bullets_callback(){
+            ?>
+            
+                <input 
+                    type="checkbox" 
+                    name="mv_slider_options[mv_slider_bullets]" 
+                    id="mv_slider_bullets" 
+                    value="1"
+                    <?php
+                        if(isset( self::$options['mv_slider_bullets'] ) ){
+                            checked( "1",self::$options['mv_slider_bullets'], true );
+                        }
+                    ?>
+                />
+                <label for="mv_slider_bullets">Whether to display bullets or not</label>
+            <?php
+
+        }
+
+        public function mv_slider_style_callback(){
+            ?>
+            <select
+                id="mv_slider_style"
+                name="mv_slider_options[mv_slider_style]">
+                <option value="style-1"
+                    <?php isset( self::$options['mv_slider_style'] ) ? selected( 'style-1', self::$options['mv_slider_style'], true) : '';?>>Style-1</option>
+                <option value="style-2"
+                    <?php isset( self::$options['mv_slider_style'] ) ? selected( 'style-2', self::$options['mv_slider_style'], true) : '';?>>Style-2</option>
+            </select>
+            <?php
+        }
+
     }
 }
